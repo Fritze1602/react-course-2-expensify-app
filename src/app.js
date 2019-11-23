e8824db08eec670c0485cc2caffd40dd96296be4
Friedrich Siever <friedrich.siever@gmail.com>
2019-11-23 14:25:05 +0100

    startApplication with data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './stores/configureStore';
 
+import {startSetExpenses} from './actions/expenses'
+
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -19,4 +22,9 @@ const jsx = (
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render('loading ...', document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx, document.getElementById('app'));
+})
+
